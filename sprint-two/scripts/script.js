e7248ed61a9d5be6e4f11c2e9a32956558d24332
ezekielim <change.3602@gmail.com>
2020-05-03 23:42:50 -0700

    changes to sprint two js

---
+++ sprint-two/scripts/script.js
@@ -0,0 +1,37 @@
+let showsTable = {
+  rowOne: {
+    cellOne: "Mon Dec 17 2018",
+    cellTwo: "Ronald Lane",
+    CellThree: "San Francisco, CA",
+  },
+  rowTwo: {
+    cellOne: "Tue Jul 18 2019",
+    cellTwo: "Pier 3 East",
+    CellThree: "San Francisco, CA",
+  },
+  rowThree: {
+    cellOne: "Fri Jul 22 2019",
+    cellTwo: "View Lounge",
+    CellThree: "San Francisco, CA",
+  },
+  rowFour: {
+    cellOne: "Sat Aug 12 2019",
+    cellTwo: "Hyatt Agency",
+    CellThree: "San Francisco, CA",
+  },
+  rowFive: {
+    cellOne: "Fri Sep 05 2019",
+    cellTwo: "Moscow Center",
+    CellThree: "San Francisco, CA",
+  },
+  rowSix: {
+    cellOne: "Fri Sep 05 2019",
+    cellTwo: "Hyatt Agency",
+    CellThree: "San Francisco, CA",
+  },
+};
+function genTable(showsTable) {
+  let tbl = document.getElementById("shows-table");
+  for (i = 0; i < showsTable.length; i++) {}
+}
+genTable(showsTable);
