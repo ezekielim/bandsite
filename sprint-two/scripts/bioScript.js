514b36853d01cbdd43e3d69721ccd90515574885
ezekielim <change.3602@gmail.com>
2020-05-04 13:39:56 -0700

    updates to dynamic comment js

---
+++ sprint-two/scripts/bioScript.js
@@ -0,0 +1,65 @@
+/* --------- BIOGRAPHY COMMENT SECTION --------- */
+const genComments = [
+  {
+    name: "Michael Lyons",
+    date: new Date("2018-12-18"),
+    message:
+      "They BLEW the ROOF off at their last show, once everyone started ﬁguring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed",
+  },
+  {
+    name: "Gary Wong",
+    date: new Date("2018-12-12"),
+    message:
+      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+  },
+  {
+    name: "Theodore Duncan",
+    date: new Date("2018-11-15"),
+    message:
+      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s deﬁnitely my favorite ever",
+  },
+];
+
+const commentForm = document.getElementById("form-id");
+const commentList = document.getElementById("comment-list-id");
+commentList.setAttribute("class", "commentList");
+commentForm.addEventListener("submit", createComment);
+//fn for displaying the array objects
+function displayComments(genComments) {
+  commentList.innerHTML = "";
+  for (const i in genComments) {
+    let listComment = document.createElement("li");
+    let msgName = document.createElement("h3");
+    msgName.innerText = genComments[i].name;
+    let msgDisc = document.createElement("p");
+    msgDisc.innerText = genComments[i].message;
+
+    let msgTime = document.createElement("time");
+    msgTime.innerText = genComments[i].date.getDay();
+    msgTime.innerText += ", " + genComments[i].date.getMonth();
+    msgTime.innerText += ", " + genComments[i].date.getFullYear();
+
+    listComment.appendChild(msgName);
+    listComment.appendChild(msgDisc);
+    listComment.appendChild(msgTime);
+    commentList.appendChild(listComment);
+  }
+}
+
+function createComment(event) {
+  event.preventDefault();
+
+  let nameComment = event.target.name.value;
+  let msgComment = event.target.comment.value;
+  let dateComment = new Date();
+  if (nameComment !== "" && msgComment !== "") {
+    genComments.push({
+      name: nameComment,
+      date: msgComment,
+      message: dateComment,
+    });
+    displayComments(genComments);
+  }
+}
+
+displayComments(genComments);
--- sprint-two/scripts/showScript.js
+++ sprint-two/scripts/showScript.js
@@ -1,9 +1,7 @@
 /* --------- SHOWS PAGE --------- */
 let showsTable = [
-  {
-    //array table
-    row1: ["Mon Dec 17 2018", "Ronald Lane", "San Francisco, CA"].join(" "),
-  },
+  //array table
+  ["Mon Dec 17 2018", "Ronald Lane", "San Francisco, CA"].join(" "),
   ["Tue Jul 18 2019", "Pier 3 East", "San Francisco, CA"].join(" "),
   ["Tue Jul 18 2019", "Pier 3 East", "San Francisco, CA"].join(" "),
   ["Fri Jul 22 2019", "View Lounge", "San Francisco, CA"].join(" "),
@@ -37,28 +35,3 @@ function genTable(showsTable) {
 }
 genTable(showsTable);
 /* --------- END OF SHOWS PAGE --------- */
-
-/* --------- BIOGRAPHY COMMENT SECTION --------- */
-let genComments = [
-  {
-    name: "Michael Lyons",
-    date: new Date(2018 - 12 - 18),
-    message:
-      "They BLEW the ROOF off at their last show, once everyone started ﬁguring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed",
-  },
-  {
-    name: "Gary Wong",
-    date: new Date(2018 - 12 - 12),
-    message:
-      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-  },
-  {
-    name: "Theodore Duncan",
-    date: new Date(2018 - 11 - 15),
-    message:
-      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s deﬁnitely my favorite ever",
-  },
-];
-
-let commentform = document.getElementById("comment-form-id");
-function displayComments() {}
