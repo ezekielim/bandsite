3138f87a64c374dc7438128e9f02eda8452a7f48
ezekielim <change.3602@gmail.com>
2020-05-04 04:17:47 -0700

    updated js for comments

--- sprint-two/scripts/script.js
+++ sprint-two/scripts/script.js
@@ -1,37 +1,41 @@
-let showsTable = {
-  rowOne: {
-    cellOne: "Mon Dec 17 2018",
-    cellTwo: "Ronald Lane",
-    CellThree: "San Francisco, CA",
+/* --------- SHOWS PAGE --------- */
+let showsTable = [
+  {
+    //array table
+    row1: ["Mon Dec 17 2018", "Ronald Lane", "San Francisco, CA"].join(" "),
   },
-  rowTwo: {
-    cellOne: "Tue Jul 18 2019",
-    cellTwo: "Pier 3 East",
-    CellThree: "San Francisco, CA",
-  },
-  rowThree: {
-    cellOne: "Fri Jul 22 2019",
-    cellTwo: "View Lounge",
-    CellThree: "San Francisco, CA",
-  },
-  rowFour: {
-    cellOne: "Sat Aug 12 2019",
-    cellTwo: "Hyatt Agency",
-    CellThree: "San Francisco, CA",
-  },
-  rowFive: {
-    cellOne: "Fri Sep 05 2019",
-    cellTwo: "Moscow Center",
-    CellThree: "San Francisco, CA",
-  },
-  rowSix: {
-    cellOne: "Fri Sep 05 2019",
-    cellTwo: "Hyatt Agency",
-    CellThree: "San Francisco, CA",
-  },
-};
+  ["Tue Jul 18 2019", "Pier 3 East", "San Francisco, CA"].join(" "),
+  ["Tue Jul 18 2019", "Pier 3 East", "San Francisco, CA"].join(" "),
+  ["Fri Jul 22 2019", "View Lounge", "San Francisco, CA"].join(" "),
+  ["Sat Aug 12 2019", "Hyatt Agency", "San Francisco, CA"].join(" "),
+  ["Fri Sep 05 2019", "Moscow Center", "San Francisco, CA"].join(" "),
+  ["Fri Sep 05 2019", "Hyatt Agency", "San Francisco, CA"].join(" "),
+];
+
 function genTable(showsTable) {
-  let tbl = document.getElementById("shows-table");
-  for (i = 0; i < showsTable.length; i++) {}
+  //fn to append array strings to DOM
+  let tbl = document.getElementById("show-table-id");
+  let tblhead = document.createElement("thead");
+  tblhead.innerHTML = ["DATES", "VENUE", "LOCATION"].join(" ");
+  tblhead.setAttribute("class", "columns");
+  tbl.appendChild(tblhead);
+  for (i in showsTable) {
+    let rows = document.createElement("tr");
+    rows.setAttribute("class", "rows");
+    let rowCell = rows.insertCell();
+    let rowTd = document.createElement("td");
+    rowTd.setAttribute("class", "rowTd");
+    rowCell.appendChild(rowTd);
+    rowTd.innerHTML = showsTable[i];
+    tbl.appendChild(rows);
+    let btn = document.createElement("button");
+    btn.setAttribute("class", "buyBtn");
+    btn.innerText = "BUY TICKETS";
+    rows.appendChild(btn);
+    console.log(showsTable[i]);
+  }
 }
 genTable(showsTable);
+/* --------- END OF SHOWS PAGE --------- */
+
+/* --------- BIOGRAPHY COMMENT SECTION --------- */
